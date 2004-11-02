0e01c50289514e5177bae6c3b4cd90f1b4c7fc53
Dietrich Moerman <dietrich.moerman@gmail.com>
2004-11-02 16:33:35 +0000

    new logos

--- templates/default/global.php
+++ templates/default/global.php
@@ -75,7 +75,7 @@ $templates['normal_header'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Tran
 	
 	<table class="header">
 		<tr>
-			<td class="logo"><a href="{link_home}"><img src="{img_dir}usebb.gif" alt="UseBB" title="{home}" /></a></td>
+			<td class="logo"><a href="{link_home}"><img src="{img_dir}usebb.png" alt="UseBB" title="{home}" /></a></td>
 			<td class="namebox" nowrap="nowrap"><div class="title">{board_name}</div><div class="descr">{board_descr}</div></td>
 		</tr>
 	</table>
@@ -90,7 +90,7 @@ $templates['normal_footer'] = '
 		{link_bar}
 	</div>
 	<div class="banners">
-		<a href="http://www.usebb.net"><img src="{img_dir}powered-by-usebb.gif" alt="Powered by UseBB" /></a>
+		<a href="http://www.usebb.net"><img src="{img_dir}powered-by-usebb.png" alt="Powered by UseBB" /></a>
 		<a href="http://validator.w3.org/check/referer"><img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0!" /></a>
 		<a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="http://jigsaw.w3.org/css-validator/images/vcss" alt="Valid CSS!" /></a>
 		<a href="http://www.php.net"><img src="{img_dir}php-power-white.gif" alt="Powered by PHP" /></a>

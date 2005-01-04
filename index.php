d41bf60f820810fefecedfb7e8f609e144ee111c
Dietrich Moerman <dietrich.moerman@gmail.com>
2005-01-04 18:00:44 +0000

    tpl improvements + index.php partial rewrite

--- index.php
+++ index.php
@@ -70,63 +70,71 @@ if ( !$functions->get_stats('forums') ) {
 	if ( !($result = $db->query("SELECT f.id, f.name, f.descr, f.status, f.topics, f.posts, f.auth, c.id AS cat_id, c.name AS cat_name, t.topic_title, t.last_post_id, t.count_replies, p.poster_id, p.poster_guest, p.post_time, u.name AS poster_name, u.level AS poster_level FROM ( ( ( ".TABLE_PREFIX."forums f LEFT JOIN ".TABLE_PREFIX."topics t ON f.last_topic_id = t.id ) LEFT JOIN ".TABLE_PREFIX."posts p ON t.last_post_id = p.id ) LEFT JOIN ".TABLE_PREFIX."members u ON p.poster_id = u.id ), ".TABLE_PREFIX."cats c WHERE f.cat_id = c.id ORDER BY c.sort_id ASC, c.id ASC, f.sort_id ASC, f.id ASC")) )
 		$functions->usebb_die('SQL', 'Unable to get forums and categories!', __FILE__, __LINE__);
 	
-	//
-	// This array holds a list of categories that have been parsed
-	//
-	$seen_cats = array();
-	
-	//
-	// This will set TRUE when viewable forums have been found
-	// Until now, it remains FALSE
-	//
-	$viewableforums = FALSE;
-	
-	//
-	// Loop through the forums
-	//
+	$categories = array();
+	$forums = array();
 	while ( $forumdata = $db->fetch_result($result) ) {
-		//
-		// If this user can view this forum
-		//
+		
 		if ( $functions->auth($forumdata['auth'], 'view', $forumdata['id']) ) {
 			
-			//
-			// If this is the only (viewable) forum and the forum has been set up
-			// to kick the user to this only (viewable) forum ...
-			//
-			if ( $functions->get_config('kick_user_to_only_viewable_forum') && intval($functions->get_stats('forums')) === 1 ) {
-				
-				header('Location: '.$functions->get_config('board_url').$functions->make_url('forum.php', array('id' => $forumdata['id']), false));
-				exit();
-				
-			}
+			if ( !array_key_exists($forumdata['cat_id'], $categories) )
+				$categories[$forumdata['cat_id']] = 1;
+			else
+				$categories[$forumdata['cat_id']]++;
+			$forums[] = $forumdata;
 			
-			if ( !isset($headershown) ) {
-				
-				$location_bar = '<a href="'.$functions->make_url('index.php').'">'.htmlentities($functions->get_config('board_name')).'</a>';
-				$template->parse('location_bar', 'global', array(
-					'location_bar' => $location_bar
-				));
-				
-				//
-				// A forumlist heading needs to be parsed because there is at least
-				// one viewable forum. Parse it if this hasn't been done yet.
-				//
-				$template->parse('forumlist_header', 'forumlist', array(
-					'forum' => $lang['Forum'],
-					'topics' => $lang['Topics'],
-					'posts' => $lang['Posts'],
-					'latest_post' => $lang['LatestPost']
-				));
-				
-				//
-				// We have parsed the header. This doesn't need to be done again.
-				//
-				$headershown = TRUE;
-				
-			}
+		}
+		
+	}
+	
+	if ( !count($forums) ) {
+		
+		//
+		// There were no viewable forums.
+		// Show this notice
+		//
+		$template->parse('msgbox', 'global', array(
+			'box_title' => $lang['Note'],
+			'content' => $lang['NoViewableForums']
+		));
+		
+	} elseif ( $functions->get_config('kick_user_to_only_viewable_forum') && count($forums) === 1 ) {
+		
+		//
+		// If this is the only (viewable) forum and the forum has been set up
+		// to kick the user to this only (viewable) forum ...
+		//
+		header('Location: '.$functions->get_config('board_url').$functions->make_url('forum.php', array('id' => $forumdata['id']), false));
+		exit();
+		
+	} else {
 			
-			if ( empty($seen_cats[$forumdata['cat_id']]) ) {
+		$location_bar = '<a href="'.$functions->make_url('index.php').'">'.htmlentities($functions->get_config('board_name')).'</a>';
+		$template->parse('location_bar', 'global', array(
+			'location_bar' => $location_bar
+		));
+		$template->parse('forumlist_header', 'forumlist', array(
+			'forum' => $lang['Forum'],
+			'topics' => $lang['Topics'],
+			'posts' => $lang['Posts'],
+			'latest_post' => $lang['LatestPost']
+		));
+		
+		$seen_forums = array();
+		
+		//
+		// Loop through the forums
+		//
+		foreach ( $forums as $forumdata ) {
+		
+			//
+			// Which forum in the current category is this?
+			//
+			if ( empty($seen_forums[$forumdata['cat_id']]) )
+				$seen_forums[$forumdata['cat_id']] = 1;
+			else
+				$seen_forums[$forumdata['cat_id']]++;
+			
+			if ( $seen_forums[$forumdata['cat_id']] === 1 ) {
 				
 				//
 				// If we didn't parse this category yet, than do it now
@@ -217,10 +225,10 @@ if ( !$functions->get_stats('forums') ) {
 				
 			}
 			
-			if ( empty($seen_cats[$forumdata['cat_id']]) ) {
+			if ( $seen_forums[$forumdata['cat_id']] === $categories[$forumdata['cat_id']] ) {
 				
 				//
-				// If we didn't parse this category yet, than do it now
+				// If we didn't parse this category footer yet, than do it now
 				//
 				$template->parse('forumlist_cat_footer', 'forumlist', array(
 					'forum' => $lang['Forum'],
@@ -230,38 +238,12 @@ if ( !$functions->get_stats('forums') ) {
 					'cat_name' => '<a href="'.$functions->make_url('index.php', array('cat' => $forumdata['cat_id'])).'#cat'.$forumdata['cat_id'].'" name="cat'.$forumdata['cat_id'].'">'.htmlentities(stripslashes($forumdata['cat_name'])).'</a>'
 				));
 				
-				//
-				// *Now* we've seen this category... :)
-				//
-				$seen_cats[$forumdata['cat_id']] = TRUE;
-				
 			}
-			
-			//
-			// There is at least one viewable forum, so set this TRUE.
-			//
-			$viewableforums = TRUE;
 			
 		}
 		
-	}
-	
-	if ( !$viewableforums ) {
-		
 		//
-		// There were no viewable forums.
-		// Show this notice
-		//
-		$template->parse('msgbox', 'global', array(
-			'box_title' => $lang['Note'],
-			'content' => $lang['NoViewableForums']
-		));
-		
-	} else {
-		
-		//
-		// There were viewable forums,
-		// so parse the forumlist footer
+		// Parse the forumlist footer
 		//
 		$template->parse('forumlist_footer', 'forumlist');
 		

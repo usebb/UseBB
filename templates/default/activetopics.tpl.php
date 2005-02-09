4f5435e210f3a504636b6489b14c7ef700516382
Dietrich Moerman <dietrich.moerman@gmail.com>
2005-02-09 14:39:00 +0000

    damn, forgot this one

---
+++ templates/default/activetopics.tpl.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+	Copyright (C) 2003-2005 UseBB Team
+	http://www.usebb.net
+	
+	$Header$
+	
+	This file is part of UseBB.
+	
+	UseBB is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+	
+	UseBB is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with UseBB; if not, write to the Free Software
+	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA	02111-1307	USA
+*/
+
+//
+// Die when called directly in browser
+//
+if ( !defined('INCLUDED') )
+	exit();
+
+//
+// Topic list templates
+//
+
+$templates['topiclist_header'] = '
+	<table class="maintable">
+		<tr>
+			<td colspan="7" class="forumcat">&raquo; {active_topics}</td>
+		</tr>
+		<tr>
+			<th></th>
+			<th>{topic}</th>
+			<th>{forum}</th>
+			<th>{author}</th>
+			<th>{replies}</th>
+			<th>{views}</th>
+			<th>{latest_post}</th>
+		</tr>
+';
+
+$templates['topiclist_topic'] = '
+		<tr>
+			<td class="icon"><img src="{img_dir}{topic_icon}" alt="{topic_status}" /></td>
+			<td>{topic_name}<div class="topicpagelinks">{topic_page_links}</div></td>
+			<td>{forum}</td>
+			<td class="author">{author}</td>
+			<td class="count">{replies}</td>
+			<td class="count">{views}</td>
+			<td class="lastpostinfo">{by_author} <a href="{last_post_url}">&gt;&gt;</a><br />{on_date}</td>
+		</tr>
+';
+
+$templates['topiclist_footer'] = '
+	</table>
+';
+
+?>

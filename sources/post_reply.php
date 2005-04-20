972d53fa95acf2e56a7dd6d4111597921d14d29a
Dietrich Moerman <dietrich.moerman@gmail.com>
2005-04-20 15:27:18 +0000

    added some tabindexes

--- sources/post_reply.php
+++ sources/post_reply.php
@@ -258,13 +258,13 @@ if ( !$db->num_rows($result) ) {
 			$template->parse('post_form', 'various', array(
 				'form_begin' => '<form action="'.$functions->make_url('post.php', array('topic' => $_GET['topic'])).'" method="post">',
 				'post_title' => $lang['PostReply'],
-				'username_input' => ( $session->sess_info['user_id'] ) ? '<a href="'.$functions->make_url('profile.php', array('id' => $session->sess_info['user_info']['id'])).'">'.unhtml(stripslashes($session->sess_info['user_info']['displayed_name'])).'</a>' : '<input type="text" size="25" maxlength="'.$functions->get_config('username_max_length').'" name="user" value="'.unhtml(stripslashes($_POST['user'])).'" />',
+				'username_input' => ( $session->sess_info['user_id'] ) ? '<a href="'.$functions->make_url('profile.php', array('id' => $session->sess_info['user_info']['id'])).'">'.unhtml(stripslashes($session->sess_info['user_info']['displayed_name'])).'</a>' : '<input type="text" size="25" maxlength="'.$functions->get_config('username_max_length').'" name="user" value="'.unhtml(stripslashes($_POST['user'])).'" tabindex="1" />',
 				'subject_input' => '<a href="'.$functions->make_url('topic.php', array('id' => $_GET['topic'])).'">'.$topic_title.'</a>',
-				'content_input' => '<textarea rows="'.$template->get_config('textarea_rows').'" cols="'.$template->get_config('textarea_cols').'" name="content" id="tags-txtarea">'.$_POST['content'].'</textarea>',
+				'content_input' => '<textarea rows="'.$template->get_config('textarea_rows').'" cols="'.$template->get_config('textarea_cols').'" name="content" id="tags-txtarea" tabindex="2">'.$_POST['content'].'</textarea>',
 				'bbcode_controls' => $functions->get_bbcode_controls(),
 				'smiley_controls' => $functions->get_smiley_controls(),
 				'options_input' => $options_input,
-				'submit_button' => '<input type="submit" name="submit" value="'.$lang['OK'].'" />',
+				'submit_button' => '<input type="submit" name="submit" value="'.$lang['OK'].'" tabindex="3" />',
 				'preview_button' => '<input type="submit" name="preview" value="'.$lang['Preview'].'" />',
 				'reset_button' => '<input type="reset" value="'.$lang['Reset'].'" />',
 				'form_end' => '</form>'

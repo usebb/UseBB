3678792ff26a40873e5f0c5f5aa858a5b4405c4b
Dietrich Moerman <dietrich.moerman@gmail.com>
2005-07-01 09:06:41 +0000

    Smilies don't require whitespace anymore to be parsed.

--- sources/functions.php
+++ sources/functions.php
@@ -888,7 +888,7 @@ class functions {
 		if ( $smilies ) {
 			
 			foreach ( $template->get_config('smilies') as $pattern => $img )
-				$string = preg_replace('#([\s\]\[])'.preg_quote(unhtml($pattern), '#').'([\s\]\[])#', '\\1<img src="templates/'.$this->get_config('template').'/smilies/'.$img.'" alt="'.unhtml($pattern).'" />\\2', $string);
+				$string = preg_replace('#'.preg_quote(unhtml($pattern), '#').'#', '<img src="templates/'.$this->get_config('template').'/smilies/'.$img.'" alt="'.unhtml($pattern).'" />', $string);
 			
 		}
 		

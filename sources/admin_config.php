77b59ab373e4817eab287f178373c5b39c3f2368
Dietrich Moerman <dietrich.moerman@gmail.com>
2005-07-17 11:24:20 +0000

    save

--- sources/admin_config.php
+++ sources/admin_config.php
@@ -29,20 +29,41 @@
 if ( !defined('INCLUDED') )
 	exit();
 
-if ( !empty($_POST['type']) && !empty($_POST['server']) && !empty($_POST['username']) && !empty($_POST['passwd']) && !empty($_POST['dbname']) && !empty($_POST['prefix']) && !empty($_POST['active_topics_count']) && !empty($_POST['admin_email']) && preg_match(EMAIL_PREG, $_POST['admin_email']) && !empty($_POST['board_descr']) && !empty($_POST['board_keywords']) && !empty($_POST['board_name']) && !empty($_POST['date_format']) && !empty($_POST['email_view_level']) && !empty($_POST['flood_interval']) && !empty($_POST['language']) && in_array($_POST['language'], $functions->get_language_packs()) && !empty($_POST['members_per_page']) && !empty($_POST['online_min_updated']) && !empty($_POST['passwd_min_length']) && !empty($_POST['posts_per_page']) && !empty($_POST['session_max_lifetime']) && !empty($_POST['session_name']) && !empty($_POST['show_edited_message_timeout']) && !empty($_POST['template']) && in_array($_POST['template'], $functions->get_template_sets()) && !empty($_POST['timezone']) && !empty($_POST['topicreview_posts']) && !empty($_POST['topics_per_page']) && !empty($_POST['username_max_length']) && !empty($_POST['view_detailed_online_list_min_level']) && !empty($_POST['view_forum_stats_box_min_level']) && !empty($_POST['view_hidden_email_addresses_min_level']) && !empty($_POST['view_memberlist_min_level']) && !empty($_POST['view_stafflist_min_level']) && !empty($_POST['view_stats_min_level']) && !empty($_POST['view_contactadmin_min_level']) ) {
+//
+// Easily check if necessary fields are filled in
+//
+$filled_in = true;
+$necessary_settings = array(
+	'strings' => array('type', 'server', 'username', 'passwd', 'dbname', 'prefix', 'admin_email', 'board_descr', 'board_keywords', 'board_name', 'date_format', 'language', 'session_name', 'template'),
+	'integers' => array('active_topics_count', 'flood_interval', 'members_per_page', 'online_min_updated', 'passwd_min_length', 'posts_per_page', 'session_max_lifetime', 'show_edited_message_timeout', 'topicreview_posts', 'topics_per_page', 'username_max_length')
+);
+foreach ( $necessary_settings['strings'] as $key ) {
+	
+	if ( empty($_POST[$key]) )
+		$filled_in = false;
+	
+}
+foreach ( $necessary_settings['integers'] as $key ) {
+	
+	if ( empty($_POST[$key]) || !valid_int($_POST[$key]) )
+		$filled_in = false;
+	
+}
+
+if ( $filled_in && !empty($_POST['active_topics_count']) && !empty($_POST['admin_email']) && preg_match(EMAIL_PREG, $_POST['admin_email']) && !empty($_POST['email_view_level']) && !empty($_POST['flood_interval']) && !empty($_POST['language']) && in_array($_POST['language'], $functions->get_language_packs()) && !empty($_POST['members_per_page']) && !empty($_POST['online_min_updated']) && !empty($_POST['passwd_min_length']) && !empty($_POST['posts_per_page']) && !empty($_POST['session_max_lifetime']) && !empty($_POST['show_edited_message_timeout']) && !empty($_POST['template']) && in_array($_POST['template'], $functions->get_template_sets()) && !empty($_POST['timezone']) && !empty($_POST['topicreview_posts']) && !empty($_POST['topics_per_page']) && !empty($_POST['username_max_length']) && !empty($_POST['view_detailed_online_list_min_level']) && !empty($_POST['view_forum_stats_box_min_level']) && !empty($_POST['view_hidden_email_addresses_min_level']) && !empty($_POST['view_memberlist_min_level']) && !empty($_POST['view_stafflist_min_level']) && !empty($_POST['view_stats_min_level']) && !empty($_POST['view_contactadmin_min_level']) ) {
 	
 	$new_settings = array();
 	
 	//
-	// Settings represented as strings
+	// Necessary settings represented as strings
 	//
-	foreach ( array('type', 'server', 'username', 'passwd', 'dbname', 'prefix', 'admin_email', 'board_descr', 'board_keywords', 'board_name', 'date_format', 'language', 'session_name', 'template') as $setting )
+	foreach ( $necessary_settings['strings'] as $setting )
 		$new_settings[$setting] = stripslashes($_POST[$setting]);
 	
 	//
-	// Settings represented as integers
+	// Necessary settings represented as integers
 	//
-	foreach ( array('active_topics_count', 'flood_interval', 'members_per_page', 'online_min_updated', 'passwd_min_length', 'posts_per_page', 'session_max_lifetime', 'show_edited_message_timeout', 'topicreview_posts', 'topics_per_page', 'username_max_length') as $setting )
+	foreach ( $necessary_settings['integers'] as $setting )
 		$new_settings[$setting] = intval($_POST[$setting]);
 	
 	//
@@ -55,7 +76,7 @@ if ( !empty($_POST['type']) && !empty($_POST['server']) && !empty($_POST['userna
 	$admin_functions->set_config($new_settings);
 	
 }
-$admin_functions->set_config(array('friendly_urls' => 1));
+
 $admin_functions->create_body('config', '');
 
 ?>

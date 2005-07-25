4d0f79309c604bb68f077eed59d5bd60757f92e1
Dietrich Moerman <dietrich.moerman@gmail.com>
2005-07-25 11:35:28 +0000

    board config

--- sources/admin_config.php
+++ sources/admin_config.php
@@ -39,19 +39,20 @@ $necessary_settings = array(
 );
 foreach ( $necessary_settings['strings'] as $key ) {
 	
-	if ( empty($_POST[$key]) )
+	if ( empty($_POST['conf-'.$key]) )
 		$filled_in = false;
 	
 }
 foreach ( $necessary_settings['integers'] as $key ) {
 	
-	if ( !isset($_POST[$key]) || !valid_int($_POST[$key]) )
+	if ( !isset($_POST['conf-'.$key]) || !valid_int($_POST['conf-'.$key]) )
 		$filled_in = false;
 	
 }
 $user_levels = array(LEVEL_GUEST, LEVEL_MEMBER, LEVEL_MOD, LEVEL_ADMIN);
+$onoff_settings = array('allow_multi_sess', 'board_closed', 'cookie_secure', 'disable_info_emails', 'dst', 'enable_contactadmin', 'enable_detailed_online_list', 'enable_forum_stats_box', 'enable_memberlist', 'enable_quickreply', 'enable_rss', 'enable_stafflist', 'enable_stats', 'friendly_urls', 'guests_can_access_board', 'guests_can_view_profiles', 'hide_avatars', 'hide_signatures', 'hide_userinfo', 'rel_nofollow', 'return_to_topic_after_posting', 'sig_allow_bbcode', 'sig_allow_smilies', 'single_forum_mode', 'target_blank', 'users_must_activate');
 
-if ( $filled_in && preg_match(EMAIL_PREG, $_POST['admin_email']) && in_array(intval($_POST['debug']), array(0, 1, 2)) && in_array($_POST['email_view_level'], array(0, 1, 2, 3)) && in_array($_POST['language'], $functions->get_language_packs()) && in_array(intval($_POST['output_compression']), array(0, 1, 2, 3)) && in_array($_POST['template'], $functions->get_template_sets()) && isset($_POST['timezone']) && $functions->timezone_handler('check_existance', $_POST['timezone']) && in_array(intval($_POST['view_detailed_online_list_min_level']), $user_levels) && in_array(intval($_POST['view_forum_stats_box_min_level']), $user_levels) && in_array(intval($_POST['view_hidden_email_addresses_min_level']), $user_levels) && in_array(intval($_POST['view_memberlist_min_level']), $user_levels) && in_array(intval($_POST['view_stafflist_min_level']), $user_levels) && in_array(intval($_POST['view_stats_min_level']), $user_levels) && in_array(intval($_POST['view_contactadmin_min_level']), $user_levels) ) {
+if ( $filled_in && preg_match(EMAIL_PREG, $_POST['conf-admin_email']) && in_array(intval($_POST['conf-debug']), array(0, 1, 2)) && in_array($_POST['conf-email_view_level'], array(0, 1, 2, 3)) && in_array($_POST['conf-language'], $functions->get_language_packs()) && in_array(intval($_POST['conf-output_compression']), array(0, 1, 2, 3)) && in_array($_POST['conf-template'], $functions->get_template_sets()) && isset($_POST['conf-timezone']) && $functions->timezone_handler('check_existance', $_POST['conf-timezone']) && in_array(intval($_POST['conf-view_detailed_online_list_min_level']), $user_levels) && in_array(intval($_POST['conf-view_forum_stats_box_min_level']), $user_levels) && in_array(intval($_POST['conf-view_hidden_email_addresses_min_level']), $user_levels) && in_array(intval($_POST['conf-view_memberlist_min_level']), $user_levels) && in_array(intval($_POST['conf-view_stafflist_min_level']), $user_levels) && in_array(intval($_POST['conf-view_stats_min_level']), $user_levels) && in_array(intval($_POST['conf-view_contactadmin_min_level']), $user_levels) ) {
 	
 	$new_settings = array();
 	
@@ -59,61 +60,125 @@ if ( $filled_in && preg_match(EMAIL_PREG, $_POST['admin_email']) && in_array(int
 	// Necessary settings represented as strings
 	//
 	foreach ( $necessary_settings['strings'] as $setting )
-		$new_settings[$setting] = stripslashes($_POST[$setting]);
+		$new_settings[$setting] = stripslashes($_POST['conf-'.$setting]);
 	
 	//
 	// Necessary settings represented as integers
 	//
 	foreach ( $necessary_settings['integers'] as $setting )
-		$new_settings[$setting] = intval($_POST[$setting]);
+		$new_settings[$setting] = intval($_POST['conf-'.$setting]);
 	
 	//
 	// Settings which can be enabled or disabled
 	//
-	foreach ( array('allow_multi_sess', 'board_closed', 'cookie_secure', 'disable_info_emails', 'dst', 'enable_contactadmin', 'enable_detailed_online_list', 'enable_forum_stats_box', 'enable_memberlist', 'enable_quickreply', 'enable_rss', 'enable_stafflist', 'enable_stats', 'friendly_urls', 'guests_can_access_board', 'guests_can_view_profiles', 'hide_avatars', 'hide_signatures', 'hide_userinfo', 'rel_nofollow', 'return_to_topic_after_posting', 'sig_allow_bbcode', 'sig_allow_smilies', 'single_forum_mode', 'target_blank', 'users_must_activate') as $setting )
-		$new_settings[$setting] = ( !empty($_POST[$setting]) ) ? 1 : 0;
+	foreach ( $onoff_settings as $setting )
+		$new_settings[$setting] = ( !empty($_POST['conf-'.$setting]) ) ? 1 : 0;
 	
 	//
 	// Strings which can be empty
 	//
 	foreach ( array('board_closed_reason', 'board_url', 'cookie_domain', 'cookie_path', 'session_save_path') as $setting )
-		$new_settings[$setting] = ( !empty($_POST[$setting]) ) ? stripslashes($_POST[$setting]) : '';
+		$new_settings[$setting] = ( !empty($_POST['conf-'.$setting]) ) ? stripslashes($_POST['conf-'.$setting]) : '';
 	
 	//
 	// Other settings
 	//
-	$new_settings['exclude_forums_active_topics'] = ( isset($_POST['exclude_forums_active_topics']) && is_array($_POST['exclude_forums_active_topics']) ) ? $_POST['exclude_forums_active_topics'] : array();
-	$new_settings['exclude_forums_rss'] = ( isset($_POST['exclude_forums_rss']) && is_array($_POST['exclude_forums_rss']) ) ? $_POST['exclude_forums_rss'] : array();
-	$new_settings['exclude_forums_stats'] = ( isset($_POST['exclude_forums_stats']) && is_array($_POST['exclude_forums_stats']) ) ? $_POST['exclude_forums_stats'] : array();
-	$new_settings['timezone'] = (float)$_POST['timezone'];
+	$new_settings['exclude_forums_active_topics'] = ( isset($_POST['conf-exclude_forums_active_topics']) && is_array($_POST['conf-exclude_forums_active_topics']) ) ? $_POST['conf-exclude_forums_active_topics'] : array();
+	$new_settings['exclude_forums_rss'] = ( isset($_POST['conf-exclude_forums_rss']) && is_array($_POST['conf-exclude_forums_rss']) ) ? $_POST['conf-exclude_forums_rss'] : array();
+	$new_settings['exclude_forums_stats'] = ( isset($_POST['conf-exclude_forums_stats']) && is_array($_POST['conf-exclude_forums_stats']) ) ? $_POST['conf-exclude_forums_stats'] : array();
+	$new_settings['timezone'] = (float)$_POST['conf-timezone'];
 	
 	$admin_functions->set_config($new_settings);
 	
-}
-
-$content = '<p>'.$lang['ConfigInfo'].'</p>
-
-<form action="'.$functions->make_url('admin.php', array('act' => 'config')).'" method="post">
-
-<h2>'.$lang['ConfigDBConfig'].'</h2>
-<table id="adminconfigtable">
-';
-
-foreach ( $dbs as $key => $val ) {
+} else {
 	
-	$_POST[$key] = ( !empty($_POST[$key]) ) ? $_POST[$key] : $val;
+	$content = '<p>'.$lang['ConfigInfo'].'</p>
+	
+	<form action="'.$functions->make_url('admin.php', array('act' => 'config')).'" method="post">
+	
+	<h2>'.$lang['ConfigDBConfig'].'</h2>
+	<table class="adminconfigtable">
+	';
+	
+	//
+	// Easy output of database config
+	//
+	foreach ( $dbs as $key => $val ) {
+		
+		$_POST['conf-'.$key] = ( isset($_POST['conf-'.$key]) ) ? $_POST['conf-'.$key] : $val;
+		$content .= '	<tr>
+			<td class="fieldtitle">'.$lang['ConfigDB-'.$key].' <small>*</small></td><td><input type="text" size="15" name="conf-'.$key.'" value="'.unhtml(stripslashes($_POST['conf-'.$key])).'" /></td>
+		</tr>
+	';
+		
+	}
+	
 	$content .= '	<tr>
-		<td class="fieldtitle">'.$lang['ConfigDB-'.$key].'</td><td><input type="text" name="'.$key.'" value="'.$_POST[$key].'" /></td>
-	</tr>
-';
+			<td colspan="2" class="submit"><input type="submit" value="'.$lang['Send'].'" /> <input type="reset" value="'.$lang['Reset'].'" /></td>
+		</tr>
+	</table>
+	
+	<h2>'.$lang['ConfigBoardConfig'].'</h2>
+	<table class="adminconfigtable">
+	';
+	
+	//
+	// These are all the current config settings
+	// Here we build the input tags for strings and integers, except for some values
+	//
+	foreach ( $functions->board_config as $key => $val ) {
+		
+		if ( $_SERVER['REQUEST_METHOD'] == 'POST' )
+			$_POST['conf-'.$key] = ( isset($_POST['conf-'.$key]) ) ? $_POST['conf-'.$key] : '';
+		else
+			$_POST['conf-'.$key] = $val;
+		
+	}
+	
+	$input = array();
+	
+	foreach ( $necessary_settings['strings'] as $key ) {
+		
+		if ( in_array($key, array('type', 'server', 'username', 'passwd', 'dbname', 'prefix', 'language', 'template')) )
+			continue;
+		
+		$input[$key] = '	<tr>
+			<td class="fieldtitle">'.$lang['ConfigBoard-'.$key].' <small>*</small></td><td><input type="text" size="30" name="conf-'.$key.'" value="'.unhtml(stripslashes($_POST['conf-'.$key])).'" /></td>
+		</tr>
+	';
+		
+	}
+	foreach ( $necessary_settings['integers'] as $key ) {
+		
+		if ( in_array($key, array('debug', 'email_view_level', 'output_compression', 'view_detailed_online_list_min_level', 'view_forum_stats_box_min_level', 'view_hidden_email_addresses_min_level', 'view_memberlist_min_level', 'view_stafflist_min_level', 'view_stats_min_level', 'view_contactadmin_min_level')) )
+			continue;
+		
+		$input[$key] = '	<tr>
+			<td class="fieldtitle">'.$lang['ConfigBoard-'.$key].' <small>*</small></td><td><input type="text" size="5" name="conf-'.$key.'" value="'.unhtml(stripslashes($_POST['conf-'.$key])).'" /></td>
+		</tr>
+	';
+		
+	}
+	foreach ( $onoff_settings as $key ) {
+		
+		$enabled = ( !empty($_POST['conf-'.$key]) ) ? ' checked="checked"' : '';
+		$input[$key] = '	<tr>
+			<td class="fieldtitle">'.$key.'</td><td><input type="checkbox" name="conf-'.$key.'" value="1"'.$enabled.' /></td>
+		</tr>
+	';
+		
+	}
+	
+	$content .= join('', $input);
+	
+	$content .= '	<tr>
+			<td colspan="2" class="submit"><input type="submit" value="'.$lang['Send'].'" /> <input type="reset" value="'.$lang['Reset'].'" /></td>
+		</tr>
+	</table>
+	</form>
+	';
 	
 }
-
-$content .= '	<tr>
-		<td colspan="2" class="submit"><input type="submit" value="'.$lang['Send'].'" /> <input type="reset" value="'.$lang['Reset'].'" /></td>
-	</tr></table>
-
-</form>';
 
 $admin_functions->create_body('config', $content);
 

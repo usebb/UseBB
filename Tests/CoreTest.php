9695adaf33b7fd0608c6fbcb68d7782f897ba840
Dietrich Moerman <dietrich.moerman@gmail.com>
2011-11-08 21:56:12 +0100

    CoreTest with object mocking.

--- Tests/CoreTest.php
+++ Tests/CoreTest.php
@@ -3,57 +3,20 @@
 namespace UseBB\Tests;
 
 use UseBB\Core;
-use UseBB\Utils\SchemaManagement\SystemSchema;
 
 class CoreTest extends \PHPUnit_Framework_TestCase {
 	protected $core;
-	protected $plugins;
-	protected $systemSchema;
 
 	protected function setUp() {
-		$this->core = new Core($GLOBALS["dbConfig"], FALSE);
-		$this->systemSchema = new SystemSchema($this->core->getServiceRegistry());
-		$this->systemSchema->install();
-		$this->plugins = $this->core->getServiceRegistry()->get("plugins");
-	}
-	
-	protected function tearDown() {
+		$this->core = new Core($GLOBALS["dbConfig"]);
 		
-		$this->systemSchema->uninstall();
 	}
 
 	public function testErrors() {
-		$this->expectOutputRegex("#Error.+Undefined#");
+		$context = $this->getMock("UseBB\System\Context\AbstractContext");
+		$context->expects($this->once())->method("handleError");
+		$this->core->getServiceRegistry()->setServiceInstance("context", $context);
 
 		$foo += 1;
-	}
-
-	public function testErrorsWithPlugin() {
-		$this->expectOutputString("foo");
-
-		$this->plugins->register("System\Context\CLI", "error", 
-			function($c) { 
-				if ($c->get("number") == E_NOTICE) {
-					echo "foo";
-					return FALSE;
-				}
-			});
-
-		$foo += 1;
-	}
-
-	/**
-	 * Disabled this test for PHPUnit 3.6 ("You must not expect the generic exception class").
-	 * Keeping it in place also results in "Call to a member function uninstall() on a non-object"
-	 * in tearDown(). It is pretty useless after all.
-	 *
-	 * @ expectedException \Exception
-	 */
-	public function _testExceptions() {
-		// No way of testing this?
-		// PHPUnit catches all exceptions - does not reach exception handler.
-		//$this->expectOutputRegex("#Exception#");
-
-		throw new \Exception();
 	}
 }

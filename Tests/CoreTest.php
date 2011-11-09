cc94b068a77f0fdf4f5de414e0149eaf6f6af06c
Dietrich Moerman <dietrich.moerman@gmail.com>
2011-11-09 17:07:17 +0100

    Keep max chars per line.

--- Tests/CoreTest.php
+++ Tests/CoreTest.php
@@ -16,7 +16,8 @@ class CoreTest extends \PHPUnit_Framework_TestCase {
 		$context = $this->getMockBuilder("UseBB\System\Context\AbstractContext")
 			->disableOriginalConstructor()->getMock();
 		$context->expects($this->once())->method("handleError");
-		$this->core->getServiceRegistry()->setServiceInstance("context", $context);
+		$this->core->getServiceRegistry()
+			->setServiceInstance("context", $context);
 
 		$foo += 1;
 	}

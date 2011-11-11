2f894b68a71b46295b597ea26e894cd8937c6c8e
Dietrich Moerman <dietrich.moerman@gmail.com>
2011-11-11 23:08:45 +0100

    CLI test.

---
+++ System/Context/Tests/CLITest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace UseBB\System\Context\Tests;
+
+use UseBB\Tests\TestCase;
+use UseBB\System\Context\CLI;
+
+class CLITest extends TestCase {
+	protected $context;
+	
+	protected function setUp() {
+		$this->newServices();
+		$this->context = new CLI($this->getServices());
+	}
+	
+	public function testHandleRequest() {
+		$this->expectOutputRegex(
+			"#The CLI interface is not available in this version\.#");
+		
+		$this->context->handleRequest();
+	}
+	
+	public function testLanguage() {
+		$this->assertEquals("en", $this->context->getLanguage(array()));
+	}
+	
+	public function testEscape() {
+		$this->assertEquals("<a>", $this->context->escapeString("<a>"));
+	}
+	
+	public function testStringArgs() {
+		$this->assertEquals("Hello %name.",
+			$this->context->applyArgumentsToString(
+			"Hello %name.", array(
+				"%name" => "you>"
+			)));
+	}
+	
+	public function testGenerateLink() {
+		$this->assertEquals("", $this->context->generateLink(array()));
+	}
+	
+	public function testHandleError() {
+		$this->expectOutputRegex("#Some error\..*foo\.php.*5#");
+		
+		$this->context->handleError(E_NOTICE, "Some error.", 
+			"foo.php", 5, array());
+	}
+	
+	public function testHandleException() {
+		$this->expectOutputRegex("#Exception.*foo#s");
+		
+		$this->context->handleException(new \Exception("foo"));
+	}
+}

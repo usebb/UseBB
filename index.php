09dea9c921caf00b9ce71a6ce1f471b66dd008a3
Dietrich Moerman <dietrich.moerman@gmail.com>
2012-07-15 20:23:49 +0200

    Removed constant def by default uneeded.

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 
-// Location of UseBB files.
-if (!defined("USEBB_ROOT_PATH")) {
-	define("USEBB_ROOT_PATH", realpath("./"));
-}
-
-require USEBB_ROOT_PATH . "/includes/init.php";
+require "./includes/init.php";
 
 $core = new UseBB\Core($dbConfig);
 $core->handleRequest();

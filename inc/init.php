db4c919c147f4e40e52e083bf931cbde304d7373
Eric Hocking <erich@xaos-ia.com>
2014-03-29 19:09:47 -0400

    Update init.php
    Attempt to determine the location of UseBB automatically

--- inc/init.php
+++ inc/init.php
@@ -5,3 +5,21 @@ if(!defined("IN_USEBB"))
 {
 	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_USEBB is defined.");
 }
+
+
+/* Defines the root directory for UseBB.
+
+	Uncomment the below line and set the path manually
+	if you experience problems.
+
+	Always add a trailing slash to the end of the path.
+
+	* Path to your copy of UseBB
+ */
+//define('USEBB_ROOT', "./");
+
+// Attempt autodetection
+if(!defined('USEBB_ROOT'))
+{
+	define('USEBB_ROOT', dirname(dirname(__FILE__))."/");
+}

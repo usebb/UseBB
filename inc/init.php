7282c907c53dd85bf00b10e35f08d1537256fb2a
Eric Hocking <erich@xaos-ia.com>
2014-03-29 19:07:18 -0400

    Create init.php
    Init.php will load the UseBB core

---
+++ inc/init.php
@@ -0,0 +1,7 @@
+<?php
+
+// Disallow direct access to this file for security reasons
+if(!defined("IN_USEBB"))
+{
+	die("Direct initialization of this file is not allowed.<br /><br />Please make sure IN_USEBB is defined.");
+}
